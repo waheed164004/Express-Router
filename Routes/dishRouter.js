3faf323e1809cd7d7a5b84e404623f02147313cf
Abdul Waheed Makhdoom <waheed164004@gmail.com>
2023-04-11 13:04:16 +0500

    first commit

--- Routes/dishRouter.js
+++ Routes/dishRouter.js
@@ -30,5 +30,29 @@ dishRouter.route('/')
     res.end('Deleting all dishes');
   });
 
+dishRouter.route('/:dishId')
 
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+  })
+  
+.get((req,res,next) => {
+    //res.end('Will send dish ' + req.params.dishId + ' to you!');   
+    res.end(`will send dish${req.params.dishId} to you`);   //can use these both methods
+  })
+  
+.post((req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+   })
+  
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /dishes');
+  })
+  
+.delete((req, res, next) => {
+    res.end(`Deleting dish ${req.params.dishId}`);
+  });
   module.exports = dishRouter;
